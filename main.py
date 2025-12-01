a9fb67d55338b815c3e63ef096fdb2df69990c9d
Rutger de Graaf <rutger@whizzkit.nl>
2025-12-02 00:04:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from pymodbus.client.tcp import ModbusTcpClient
+import yaml
+
+
+def go(registers: dict, config: str = 'configuration.yaml') -> list:
+    client_config = yaml.safe_load(open(config))
+    device_id = client_config['slave_id']
+
+    modbus_tcp_client = ModbusTcpClient(
+        host=client_config['host'],
+        port=client_config['port']
+    )
+
+    modbus_tcp_client.connect()
+
+    register_values = [{f'{address:x}': modbus_tcp_client.read_holding_registers(
+        address,
+        count=count,
+        device_id=device_id
+    ).registers} for address, count in registers.items()]
+
+    modbus_tcp_client.close()
+
+    return register_values
+
+# According to the docs, I could send an "8" to register 1100H to "restart EMS"
+if '__main__' == __name__:
+    print(go({
+        0x102: 1,  # Battery SOC, 0.1
+        0x01b: 2,  # Active power of A phase(Grid)
+        0x01d: 2,  # Active power of B phase(Grid)
+        0x01f: 2,  # Active power of C phase(Grid)
+        0x111A: 1,  # EMS BOOT Version High
+        0x111B: 1,  # EMS BOOT Version Middle
+        0x111C: 1,  # EMS BOOT Version Low
+        0x11B8: 1,  # Grid Freq
+    }))
