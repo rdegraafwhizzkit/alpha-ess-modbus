693d264041e453874289f9702275f9635b27b951
Rutger de Graaf <rutger@whizzkit.nl>
2025-12-29 13:10:51 +0100

    WIP

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ from pymodbus.client.tcp import ModbusTcpClient
 import yaml
 
 
-def get_register_values(registers: dict, config: str = 'configuration.yaml') -> list:
+def get_modbus_client(config: str = 'configuration.yaml'):
     client_config = yaml.safe_load(open(config))
-    device_id = client_config['device_id']
-
-    modbus_tcp_client = ModbusTcpClient(
+    return ModbusTcpClient(
         host=client_config['host'],
         port=client_config['port']
     )
 
+
+def write_register_values(registers: dict, config: str = 'configuration.yaml'):
+    modbus_tcp_client = get_modbus_client(config)
+    raise NotImplementedError()
+
+
+def get_register_values(registers: dict, config: str = 'configuration.yaml') -> list:
+    modbus_tcp_client = get_modbus_client(config)
+
     modbus_tcp_client.connect()
 
-    register_values = [{f'{address:x}': modbus_tcp_client.read_holding_registers(
+    register_values = [{f'{address}': modbus_tcp_client.read_holding_registers(
         address,
         count=count,
-        device_id=device_id
+        # device_id=device_id
     ).registers} for address, count in registers.items()]
 
     modbus_tcp_client.close()
@@ -25,13 +32,4 @@ def get_register_values(registers: dict, config: str = 'configuration.yaml') -> 
 
 
 if '__main__' == __name__:
-    print(get_register_values({
-        0x102: 1,  # Battery SOC, 0.1
-        0x01b: 2,  # Active power of A phase(Grid)
-        0x01d: 2,  # Active power of B phase(Grid)
-        0x01f: 2,  # Active power of C phase(Grid)
-        0x111A: 1,  # EMS BOOT Version High
-        0x111B: 1,  # EMS BOOT Version Middle
-        0x111C: 1,  # EMS BOOT Version Low
-        0x11B8: 1,  # Grid Freq
-    }))
+    print({i:1 for i in range(256)})
